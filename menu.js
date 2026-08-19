ce31438684ddee7402b0639ca2d4f4d9eb86dcab
wilsonsantanna <74508180+wilsonsantanna@users.noreply.github.com>
2026-08-19 17:28:00 -0300

    menu teste

--- menu.js
+++ menu.js
@@ -14,6 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((html) => {
       // 3. Injeta o HTML dentro da div reservada
       document.getElementById("header-placeholder").innerHTML = html;
+      // Pega o caminho da URL atual (ex: /sobre.html)
+      const currentPath = window.location.pathname;
+      links.forEach((link) => {
+        // Pegamos apenas o caminho (pathname) do link para ignorar o domínio (https://...)
+        const linkPath = new URL(link.href).pathname;
+        // Verificamos se o caminho atual é igual ao do link.
+        // Também cobrimos o caso da página inicial, que pode ser "/" ou "/index.html"
+        if (
+          currentPath === linkPath ||
+          (currentPath === "/" && linkPath === "/index.html")
+        ) {
+          link.classList.add("current");
+        }
+      });
     })
     .catch((error) => console.error("Erro:", error));
 });
